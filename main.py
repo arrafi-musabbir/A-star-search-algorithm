6b7876fdae22dabac5d6b9417f3d3a73053ece9e
Musabbir Arrafi <musabbir.arrafi@gmail.com>
2022-07-03 23:18:01 +0600

    updated main

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import A_STAR as A
 
 if __name__ == "__main__":
-    A.a_Star(input('Text file path: '), 's', 'g')
+    A.a_Star(input('Text file path: '), input('Starting node: '), input('Goal: ')
 else:
     print("Invalid Module")
